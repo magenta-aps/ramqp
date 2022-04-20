135fe88bcd0ac6fb733e0fb7a1bfdf1058c95528
Emil Madsen <sovende@gmail.com>
2022-04-20 15:39:32 +0200

    Rename function --> adaptee

--- ramqp/moqp.py
+++ ramqp/moqp.py
@@ -6,7 +6,6 @@ from functools import wraps
 from typing import Any
 from typing import Callable
 from typing import Dict
-from typing import List
 from typing import Tuple
 
 from aio_pika import IncomingMessage
