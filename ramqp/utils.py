9c4a342dee276dfd38016b71269f6bf49bd97277
Casper V. Kristensen <vk@magenta.dk>
2022-06-24 13:24:26 +0200

    [#50111] Remove unused decode_body() function

--- ramqp/utils.py
+++ ramqp/utils.py
@@ -6,9 +6,6 @@ from typing import Awaitable
 from typing import Callable
 
 import structlog
-from aio_pika import IncomingMessage
-
-from .metrics import exception_decode_counter
 
 logger = structlog.get_logger()
 
@@ -19,19 +16,3 @@ CallbackType = Callable[..., Awaitable]
 def function_to_name(function: Callable) -> str:
     """Get a uniquely qualified name for a given function."""
     return function.__name__
-
-
-def decode_body(message: IncomingMessage) -> str:
-    """Decode the message body into a string.
-
-    Args:
-        message: The aio_pika message.
-
-    Returns:
-        The decoded body as a string.
-    """
-    routing_key = message.routing_key
-    with exception_decode_counter.labels(routing_key).count_exceptions():
-        decoded = message.body.decode("utf-8")
-    logger.debug("Decoded body", routing_key=routing_key, decoded=decoded)
-    return decoded
