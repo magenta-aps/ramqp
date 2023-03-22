463488584b43905a20ccaf8004364523d8c6ec85
Casper V. Kristensen <vk@magenta.dk>
2023-03-22 13:50:22 +0100

    feat!: [#53725] Remove `message2model`
    BREAKING CHANGE:
    
    `message2model` has been superseded by `get_payload_as_type`.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,10 +5,8 @@
 from typing import Any
 
 import pytest
-from pydantic import BaseModel
 
 from ramqp.utils import message2json
-from ramqp.utils import message2model
 from tests.amqp_helpers import payload2incoming
 
 
@@ -38,55 +36,3 @@ async def test_message2json() -> None:
     with pytest.raises(TypeError) as exc_info:
         await message_callback(payload=1, message=message)
     assert "got multiple values for keyword argument 'payload'" in str(exc_info.value)
-
-
-def test_message2model_preconditions() -> None:
-    """Test message2model throws the expected exceptions."""
-
-    with pytest.raises(ValueError) as exc_info:
-        message2model(callback)
-    assert "model argument not found on message2model function" in str(exc_info.value)
-
-    # pylint: disable=unused-argument
-    async def model_callback(model) -> None:  # type: ignore
-        pass
-
-    with pytest.raises(ValueError) as exc_info:
-        message2model(model_callback)
-    assert "model argument not annotated on message2model function" in str(
-        exc_info.value
-    )
-
-    async def model_annotated_callback(model: int, **kwargs: Any) -> dict[str, Any]:
-        return {"model": model, **kwargs}
-
-    message2model(model_annotated_callback)
-
-
-async def test_message2model() -> None:
-    """Test message2model works as expected."""
-
-    # pylint: disable=too-few-public-methods
-    class Model(BaseModel):
-        """Dummy model."""
-
-        hello: str
-
-    async def model_callback(model: Model, **kwargs: Any) -> dict[str, Any]:
-        return {"model": model, **kwargs}
-
-    message = payload2incoming({"hello": "world"})
-
-    with pytest.raises(TypeError) as exc_info:
-        # pylint: disable=no-value-for-parameter
-        await model_callback(message=message)  # type: ignore
-    assert "missing 1 required positional argument: 'model'" in str(exc_info.value)
-
-    model_callback = message2model(model_callback)
-
-    result = await model_callback(message=message)
-    assert result == {"model": Model(hello="world"), "message": message}
-
-    with pytest.raises(TypeError) as exc_info:
-        await model_callback(message=message, model=1)
-    assert "got multiple values for keyword argument 'model'" in str(exc_info.value)
