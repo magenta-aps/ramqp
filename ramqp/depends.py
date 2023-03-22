df0ed38a7380624ea9e3a0a61b76551d4f9196ad
Casper V. Kristensen <vk@magenta.dk>
2023-03-22 14:25:10 +0100

    chore: [#53725] Move `handle_exclusively` to depends.py

--- ramqp/depends.py
+++ ramqp/depends.py
@@ -2,15 +2,21 @@
 # SPDX-License-Identifier: MPL-2.0
 """This module implement FastAPI dependency injection for RAMQP."""
 import asyncio
+from collections import defaultdict
 from collections.abc import AsyncGenerator
+from collections.abc import Awaitable
 from collections.abc import Callable
+from collections.abc import Hashable
+from contextlib import asynccontextmanager
 from contextlib import AsyncExitStack
 from functools import wraps
 from typing import Annotated
 from typing import Any
 from typing import cast
+from typing import DefaultDict
 from typing import TypeVar
 
+import anyio
 from aio_pika import IncomingMessage
 from fastapi import Depends
 from fastapi import Request
@@ -197,6 +203,57 @@ def get_payload_as_type(type_: type[T]) -> Callable[..., T]:
         return parse_raw_as(type_, payload)
 
     return inner
+
+
+def handle_exclusively(key: Callable[..., Hashable]) -> Callable:
+    """Avoids race conditions in handlers by ensuring exclusivity for arguments.
+
+    This decorator is used to ensure that the "same" message cannot be handled by a
+    message handler more than once at the same time. Here, the "same" message refers
+    to a message with arguments which would make the handling functionally identical,
+    e.g. a MO message with identical `uuid` and `object_uuid`.
+
+    Examples:
+        Simple usage::
+
+            @handle_exclusively(key=lambda x, y, z: x, y)
+            async def f(x, y, z):
+                pass
+
+            await f(x=1, y=2, z=8)
+            await f(x=3, y=4, z=8)  # accepted
+            await f(x=1, y=2, z=9)  # blocked
+
+    Args:
+        key: A custom key function returning the arguments considered when determining
+             exclusivity.
+
+    Returns:
+        Decorator to be applied to callback functions.
+    """
+    locks: DefaultDict[Hashable, anyio.Lock] = defaultdict(anyio.Lock)
+
+    @asynccontextmanager
+    async def named_lock(key: Hashable) -> AsyncGenerator[None, None]:
+        lock = locks[key]
+        async with lock:
+            try:
+                yield
+            finally:
+                # Garbage collect lock if no others are waiting to acquire.
+                # This MUST be done before we release the lock.
+                if not lock.statistics().tasks_waiting:
+                    del locks[key]
+
+    def wrapper(coro: Callable[..., Awaitable[T]]) -> Callable[..., Awaitable[T]]:
+        @wraps(coro)
+        async def wrapped(*args: Any, **kwargs: Any) -> T:
+            async with named_lock(key=key(*args, **kwargs)):
+                return await coro(*args, **kwargs)
+
+        return wrapped
+
+    return wrapper
 
 
 def sleep_on_error(delay: int = 30) -> Callable[[], AsyncGenerator[None, None]]:
